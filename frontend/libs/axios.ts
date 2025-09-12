f129411f9459bf49545359eff7cda17c7b0b71b6
pmtking <mohamadtaher6674@gmail.com>
2025-09-12 15:50:57 +0330

    fix

--- frontend/libs/axios.ts
+++ frontend/libs/axios.ts
@@ -5,7 +5,7 @@ import axios, {
 } from "axios";
 
 const api = axios.create({
-  baseURL: "http://172.24.32.1:4000",
+  baseURL: "https://api.df-neyshabor.ir/",
   timeout: 2000,
   headers: {
     "Content-Type": "application/json",
