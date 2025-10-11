66516a64e7bde938fcfbec2b63416869b16cf086
pmtking <mohamadtaher6674@gmail.com>
2025-10-11 19:48:12 +0330

    fix this code

--- backend/src/app.ts
+++ backend/src/app.ts
@@ -36,19 +36,19 @@ app.use(
 );
 
 // تابع برای گرفتن IPهای سرور
-// const getServerIPs = () => {
-//   const nets = os.networkInterfaces();
-//   const results: string[] = [];
+const getServerIPs = () => {
+  const nets = os.networkInterfaces();
+  const results: string[] = [];
 
-//   for (const name of Object.keys(nets)) {
-//     for (const net of nets[name] || []) {
-//       if (net.family === "IPv4" && !net.internal) {
-//         results.push(net.address);
-//       }
-//     }
-//   }
-//   return results;
-// };
+  for (const name of Object.keys(nets)) {
+    for (const net of nets[name] || []) {
+      if (net.family === "IPv4" && !net.internal) {
+        results.push(net.address);
+      }
+    }
+  }
+  return results;
+};
 
 // تابع راه‌اندازی
 export const startServer = async () => {
